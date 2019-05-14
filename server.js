3dfdf3556bb7b65009cf9260801a7b804145c2ce
Ko Seoyoon <rsykoss@gmail.com>
2019-05-14 16:39:04 +0800

    fix: created link to angular build library

--- server.js
+++ server.js
@@ -11,8 +11,13 @@ app.use(bodyParser.json());
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
 
+// Create link to Angular build directory
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/users", function (err, client) {
   if (err) {
     console.log(err);
     process.exit(1);
